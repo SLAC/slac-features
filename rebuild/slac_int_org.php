6ea9f4838bffe86dcd1d6313b69a9f1a5f3c58d3
Alexandru Vrabie <propeople@220v.(none)>
2013-09-06 17:48:53 +0300

    Task #7692: I have rename title from script for each profile

--- rebuild/slac_int_org.php
+++ rebuild/slac_int_org.php
@@ -9,7 +9,7 @@ $commands = array(
   ),
   array(
     'description' => 'Install selected profile: ',
-    'command' => 'drush si slac_int_org --db-url=mysql://slac_features:SwHbNJEZ6QRxrlU@localhost/slac_features --account-name=admin --account-pass=618hWVCDmY1n3uf --account-mail=admin@example.com --site-name=SLAC-IR-' . date(DATE_ATOM) . ' -y'
+    'command' => 'drush si slac_int_org --db-url=mysql://slac_features:SwHbNJEZ6QRxrlU@localhost/slac_features --account-name=admin --account-pass=618hWVCDmY1n3uf --account-mail=admin@example.com --site-name=SLAC-INT-ORG-' . date(DATE_ATOM) . ' -y'
   ),
 );
 
--- rebuild/slac_project.php
+++ rebuild/slac_project.php
@@ -9,7 +9,7 @@ $commands = array(
   ),
   array(
     'description' => 'Install selected profile: ',
-    'command' => 'drush si slac_project --db-url=mysql://slac_features:SwHbNJEZ6QRxrlU@localhost/slac_features --account-name=admin --account-pass=618hWVCDmY1n3uf --account-mail=admin@example.com --site-name=SLAC-IR-' . date(DATE_ATOM) . ' -y'
+    'command' => 'drush si slac_project --db-url=mysql://slac_features:SwHbNJEZ6QRxrlU@localhost/slac_features --account-name=admin --account-pass=618hWVCDmY1n3uf --account-mail=admin@example.com --site-name=SLAC-PROJECT-' . date(DATE_ATOM) . ' -y'
   ),
 );
 
