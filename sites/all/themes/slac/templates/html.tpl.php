01bb7d89d68b786b309495317db03c9880bb04fc
Eugen Taracila <taracilaeugen@gmail.com>
2013-11-21 13:48:27 -0700

    Theme update.

--- sites/all/themes/slac/templates/html.tpl.php
+++ sites/all/themes/slac/templates/html.tpl.php
@@ -1,55 +1,32 @@
-<?php
-/**
- * @file
- * Returns the HTML for the basic html structure of a single Drupal page.
- *
- * Complete documentation for this file is available online.
- * @see https://drupal.org/node/1728208
- */
-?><!DOCTYPE html>
-<!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?>><![endif]-->
-<!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes; ?>><![endif]-->
-<!--[if (IE 7)&(!IEMobile)]><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
-<!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes; ?>><![endif]-->
-<!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html <?php print $html_attributes . $rdf_namespaces; ?>><!--<![endif]-->
-
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
-  <?php print $head; ?>
-  <title><?php print $head_title; ?></title>
-
-  <?php if ($default_mobile_metatags): ?>
-    <meta name="MobileOptimized" content="width">
-    <meta name="HandheldFriendly" content="true">
-    <meta name="viewport" content="width=device-width">
-  <?php endif; ?>
-  <meta http-equiv="cleartype" content="on">
-	<script src="/sites/all/themes/slac/js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-	
-  <?php print $styles; ?>
-
-
-
-  <?php print $scripts; ?>
-
-
-
+<?php print $head; ?>
+<title><?php print $head_title; ?></title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width">
+<link type="text/css" rel="stylesheet" href="/sites/all/themes/slac/css/styles.css" media="all" />
+<script src="/sites/all/themes/slac/js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+<?php print $styles; ?>
+<?php print $scripts; ?>
+<!--[if lt IE 9]>
+<script src="/sites/all/themes/slac/js/selectivizr-min.js"></script>
+<![endif]--> 
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
-  <?php if ($skip_link_text && $skip_link_anchor): ?>
-    <p id="skip-link">
-      <a href="#<?php print $skip_link_anchor; ?>" class="element-invisible element-focusable"><?php print $skip_link_text; ?></a>
-    </p>
-  <?php endif; ?>
-  <?php print $page_top; ?>
-  <?php print $page; ?>
-  <?php print $page_bottom; ?>
-  <script src="/sites/all/themes/slac/js/respond.min.js"></script>
-    <!--[if lt IE 9]>
-    
-    <script src="/sites/all/themes/slac/js/selectivizr-min.js"></script>
-    
-    
-    <![endif]-->  
-  
+<!--[if lt IE 7]>
+    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+<![endif]-->	
+<?php if ($skip_link_text && $skip_link_anchor): ?>
+<p id="skip-link">
+  <a href="#<?php print $skip_link_anchor; ?>" class="element-invisible element-focusable"><?php print $skip_link_text; ?></a>
+</p>
+<?php endif; ?>
+<?php print $page_top; ?>
+<?php print $page; ?>
+<?php print $page_bottom; ?>
 </body>
 </html>
