4ef10bbb4b6d6f281aa8f406b198c8df192b9b81
Eugen Taracila <taracilaeugen@gmail.com>
2013-11-28 17:30:00 -0700

    Blog. Feature. Styles.

--- sites/all/themes/slac/templates/node--blog.tpl.php
+++ sites/all/themes/slac/templates/node--blog.tpl.php
@@ -7,7 +7,7 @@
  * @see https://drupal.org/node/1728164
  */
 ?>
-<article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<article class="node-<?php print $node->nid; ?> <?php print $classes; ?>"<?php print $attributes; ?>>
 
   <?php if ($title_prefix || $title_suffix || $display_submitted || $unpublished || !$page && $title): ?>
     <header>
