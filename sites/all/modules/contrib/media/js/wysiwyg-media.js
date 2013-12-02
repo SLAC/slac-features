3a4a9e1e7cf405d1b6895647a3b0d21d706001d7
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2013-12-01 23:50:11 -0800

    Hack media module to add event on inserting an image to alter json in token.

--- sites/all/modules/contrib/media/js/wysiwyg-media.js
+++ sites/all/modules/contrib/media/js/wysiwyg-media.js
@@ -109,12 +109,18 @@ InsertMedia.prototype = {
    * tagmap.
    */
   insert: function (formatted_media) {
-    var element = Drupal.media.filter.create_element(formatted_media.html, {
+    var info = {
           fid: this.mediaFile.fid,
           view_mode: formatted_media.type,
           attributes: formatted_media.options,
           fields: formatted_media.options
-        });
+        };
+    // Trigger custom event so other modules can add data to element variable.
+    var parameters = {info: info};
+    $(document).trigger('InsertMedia.insert', [parameters]);
+
+    var element = Drupal.media.filter.create_element(formatted_media.html, info);
+
     // Get the markup and register it for the macro / placeholder handling.
     var markup = Drupal.media.filter.getWysiwygHTML(element);
 
