dccfd98fba9b6089bf1fca98a12f81eb35a39e3d
Eugen Taracila <taracilaeugen@gmail.com>
2013-12-17 09:24:04 -0700

    Task #9260 : News list page. Feature.

--- sites/all/themes/slac/templates/node--slac_news.tpl.php
+++ sites/all/themes/slac/templates/node--slac_news.tpl.php
@@ -8,12 +8,13 @@
  */
 ?>
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?>"<?php print $attributes; ?>>
-
   <?php if ($title_prefix || $title_suffix || $display_submitted || $unpublished || !$page && $title): ?>
-    <header>
-      <?php if ($content['field_slac_news_media']): ?>
+    <?php if ($content['field_slac_news_media']): ?>
+        <div class="news-image">
         <?php print render($content['field_slac_news_media']); ?>
+        </div>
       <?php endif; ?>
+      <div class="news-content">
       <?php if ($display_submitted): ?>
         <p class="submitted separator">
           <?php print $user_picture; ?>
@@ -25,22 +26,19 @@
         <h2 class='separator' <?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
-
       <?php if ($unpublished): ?>
         <mark class="unpublished"><?php print t('Unpublished'); ?></mark>
       <?php endif; ?>
-    </header>
+      
   <?php endif; ?>
 
   <?php
     hide($content['field_slac_news_media']);
     hide($content['comments']);
-    hide($content['links']);?>
-     <div class="blog-wrapper separator">
-    <?php
+    hide($content['links']);
     print render($content);
   ?>
-</div>
+  </div>
   <?php //print render($content['links']); ?>
 
   <?php //print render($content['comments']); ?>
