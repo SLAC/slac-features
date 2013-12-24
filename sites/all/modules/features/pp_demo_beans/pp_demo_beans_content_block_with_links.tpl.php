6c1cc7839e85912419973deec9ede402a37b6171
Andrei Jechiu <andrei.jechiu@wearepropeople.com>
2013-12-24 10:53:19 +0200

    Beans block types fixes.

--- sites/all/modules/features/pp_demo_beans/pp_demo_beans_content_block_with_links.tpl.php
+++ sites/all/modules/features/pp_demo_beans/pp_demo_beans_content_block_with_links.tpl.php
@@ -40,34 +40,33 @@
         </div>
       <?php endif; ?>
 
-      <div style="float: left; width: 55%;"><p>
-        <?php print $body; ?>
-      </p></div>
-
-      <?php if (isset($links_list)): ?>
+      <?php if (!empty($links_list)): ?>
         <div style="
-          background: #f6f6f6;
-          background: -moz-linear-gradient(top, #f6f6f6 0%, #ebebeb 100%);
-          background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #f6f6f6), color-stop(100%, #ebebeb));
-          background: -webkit-linear-gradient(top, #f6f6f6 0%, #ebebeb 100%);
-          background: -o-linear-gradient(top, #f6f6f6 0%, #ebebeb 100%);
-          background: -ms-linear-gradient(top, #f6f6f6 0%, #ebebeb 100%);
-          background: linear-gradient(to bottom, #f6f6f6 0%, #ebebeb 100%);
-          filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#f6f6f6', endColorstr='#ebebeb',GradientType=0 );
+          background: #DBDADB;
           width: 100%;
           padding: 1px 3%;
           font-size: 13px;
           overflow: hidden;
           margin-top: 0;
           float: right;
-          width: 30%;
-          ">
-          <?php print $links_list; ?>
+          width: 15%;
+        ">
+        <ul>
+          <?php foreach($links_list as $link_from_list): ?>
+            <li style="list-style-type: disc;">
+              <?php print $link_from_list; ?>
+            </li>
+          <? endforeach; ?>
+        </ul>
         </div>
       <?php endif; // if ($links_list) ?>
 
+      <div style="float: left; width: 83%;"><p>
+          <?php print $body; ?>
+        </p></div>
+
       <?php if ($link): ?>
-        <div><p><?php print $link; ?></p></div>
+        <p><?php print $link; ?></p>
       <?php endif; ?>
 
     </div>
