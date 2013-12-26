7d763afd3c9976832246b5fcdb509c79df2b950c
Andrei Jechiu <andrei.jechiu@wearepropeople.com>
2013-12-26 16:34:04 +0200

    Task #9581: Add classes to html elements..

--- sites/all/modules/custom/slac_share_block/slac_share_block.tpl.php
+++ sites/all/modules/custom/slac_share_block/slac_share_block.tpl.php
@@ -1,7 +1,7 @@
 <div class="share-block-wrapper">
 <?php foreach ($share_options as $option) { ?>
-  <div class="share-item">
-    <a target="_blank" href="<?php print $option['url']; ?>">
+  <div class="share-item share-item-<?php print $option['id']; ?>">
+    <a class="share-<?php print $option['id']; ?>" target="_blank" href="<?php print $option['url']; ?>">
         <?php print $option['title']; ?>
     </a>
   </div>
