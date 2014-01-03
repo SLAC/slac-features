a56d7d0705396240dd0d96932451dff125aac203
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2014-01-03 03:45:57 -0800

    Add drush uli for site rebuild script.

--- rebuild/slac_ext_org.php
+++ rebuild/slac_ext_org.php
@@ -11,10 +11,16 @@ $commands = array(
     'description' => 'Install selected profile: ',
     'command' => 'drush si slac_ext_org --db-url=mysql://slac_features:SwHbNJEZ6QRxrlU@localhost/slac_features --account-name=admin --account-pass=618hWVCDmY1n3uf --account-mail=admin@example.com --site-name=SLAC-EXT-ORG-' . date(DATE_ATOM) . ' -y'
   ),
+  array(
+    'description' => 'Admin login ',
+    'command' => 'drush uli'
+  ),
 );
 
+$command_output = array();
 foreach ($commands as $command) {
   exec($command['command'], $command_output);
 }
 
 print 'Site has been rebuilt';
+print '<pre>' . implode("\n", $command_output) . '</pre>';
