5ce1b4c67a003df383afec410f9825b46664cc86
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2014-01-14 01:26:59 -0800

    Issue #9651: Alter template for Calendar teaser to display all fields.

--- sites/all/themes/slac/templates/node--event--calendar-teaser.tpl.php
+++ sites/all/themes/slac/templates/node--event--calendar-teaser.tpl.php
@@ -1,7 +1,27 @@
 <?php
 /**
  * Template for Calendar Teaser node view mode.
+ *
+ * If you want to have more granular control over the way fields are displayed
+ * use something like follwing instead of render($content);
+ *
+ * <?php print render($content['field_slac_event_date']); ?>
+ *
+ * <?php if (isset($content['field_location'])): ?>
+ *   <?php print render($content['field_location']); ?>
+ * <?php endif; ?>
+ *
+ * <?php if (isset($content['body'])): ?>
+ *   <?php print render($content['body']); ?>
+ * <?php endif; ?>
+ *
  */
 ?>
-<a href="<?php print $node_url; ?>"><?php print $title; ?></a><br/>
-<?php print render($content['field_slac_event_date']); ?>
+<div class="event contextual-links-region">
+  <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
+  <?php print render($title_suffix); ?>
+  <br/>
+
+  <?php print render($content); ?>
+</div>
+<br/>
