8d58ff2d33c55c02aa11665e05457f15e28d7d40
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2014-01-14 00:26:26 -0800

    Issue #9651: Formatter for date in events list.

--- sites/all/themes/slac/templates/node--event--calendar-teaser.tpl.php
+++ sites/all/themes/slac/templates/node--event--calendar-teaser.tpl.php
@@ -3,4 +3,5 @@
  * Template for Calendar Teaser node view mode.
  */
 ?>
-<a href="<?php print $node_url; ?>"><?php print $title; ?></a>
+<a href="<?php print $node_url; ?>"><?php print $title; ?></a><br/>
+<?php print render($content['field_slac_event_date']); ?>
