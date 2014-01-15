d4a44ad098e2d17eaa7745a4a99bf8a740d9dfc6
Andrei Jechiu <andrei.jechiu@wearepropeople.com>
2014-01-15 14:30:31 +0200

    Task #9701: Recreate People page/structure.

---
+++ sites/all/themes/slac/templates/views-view-summary-unformatted.tpl.php
@@ -0,0 +1,25 @@
+<?php
+$letters = range('A', 'Z');
+foreach($rows as $id => $row){
+  $existing_letters[] = $row->link;
+  $urls[$row->link] = $row->url;
+}
+$classes = !empty($row_classes[$id]) ? $row_classes[$id] : '';
+print (!empty($options['inline']) ? '<span' : '<div') . ' class="views-summary views-summary-unformatted">';
+
+foreach($letters as $letter){
+  if(in_array($letter, $existing_letters)){
+    $nav[] = '<span class="result"><a href="' . $urls[$letter] . '"' . 'class="' . $classes . '">' . $letter . '</a>';
+  }
+  else {
+    $nav[] = '<span class="no-result">' . $letter . '</span>';
+  }
+}
+if (!empty($row->separator)) {
+  print implode(' ' . $row->separator . ' ', $nav);
+}
+else {
+  print implode('  ', $nav);
+}
+print !empty($options['inline']) ? '</span>' : '</div>';
+?>
