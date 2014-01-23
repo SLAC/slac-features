51aa801ef33eeaec2a66fe149bf9cefdac488286
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2014-01-23 12:18:57 -0800

    Issue #10173: Add description to Projects block.

--- sites/all/modules/custom/slac_projects/slac_projects_block.tpl.php
+++ sites/all/modules/custom/slac_projects/slac_projects_block.tpl.php
@@ -8,6 +8,11 @@
         <div class="project-image">
           <a href="<?php print $project['link']; ?>"><?php print $project['image']; ?></a>
         </div>
+        <?php if (isset($project['description'])): ?>
+          <div class="project-description">
+            <a href="<?php print $project['link']; ?>"><?php print $project['description']; ?></a>
+          </div>
+        <?php endif; ?>
       <?php else: ?>
         <div class="project-title">
           <?php print $project['title']; ?>
@@ -15,6 +20,11 @@
         <div class="project-image">
           <?php print $project['image']; ?>
         </div>
+        <?php if (isset($project['description'])): ?>
+          <div class="project-description">
+            <?php print $project['description']; ?>
+          </div>
+        <?php endif; ?>
       <?php endif; ?>
     </div>
   <?php } ?>
