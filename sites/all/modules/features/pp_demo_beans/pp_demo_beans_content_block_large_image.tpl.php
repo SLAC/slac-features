ec9c917b9059611908219f82e902b425c868287a
Eugen Taracila <taracilaeugen@gmail.com>
2014-01-31 15:11:26 -0700

    Task #10510 : ExtOrg Basic Page blocks in right sidebar.

--- sites/all/modules/features/pp_demo_beans/pp_demo_beans_content_block_large_image.tpl.php
+++ sites/all/modules/features/pp_demo_beans/pp_demo_beans_content_block_large_image.tpl.php
@@ -1,3 +1,12 @@
+<?php
+  $css_file = DRUPAL_ROOT . '/' . drupal_get_path('css', 'pp_demo_beans') . '/css/pp_demo_beans_shared.css';
+  if (file_exists($css_file)):
+?>
+  <style type="text/css">
+    <?php print file_get_contents($css_file); ?>
+  </style>
+<?php endif; ?>
+
 <div class="pp_demo-beans-image-text-link box-about box-type-1">
   <?php if ($title): ?>
     <h2><?php print $title; ?></h2>
@@ -7,7 +16,7 @@
         <div class="subtitle"><?php print $subtitle; ?></div>
       <?php endif; ?>
       <?php if (isset($image)): ?>
-        <figure><?php print $image; ?></figure>
+        <figure class="large-image"><?php print $image; ?></figure>
       <?php endif; // if ($image) ?>
       <article><?php print $body; ?></article>
   </div>
