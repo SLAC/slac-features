db344b4db9dc6c00a13a3aeccad54c80347a57ab
Eugen Taracila <taracilaeugen@gmail.com>
2014-01-31 12:05:34 -0700

    Task #10510 : ExtOrg Basic Page blocks in right sidebar.

--- sites/all/modules/features/pp_demo_beans/pp_demo_beans_content_block_with_links.tpl.php
+++ sites/all/modules/features/pp_demo_beans/pp_demo_beans_content_block_with_links.tpl.php
@@ -1,7 +1,11 @@
 <div class="pp_demo-beans-image-text-link box-about box-type-2">
-  <?php if ($title): ?><h2><?php print $title; ?></h2><? endif; ?>
+  <?php if ($title): ?>
+    <h2><?php print $title; ?></h2>
+  <? endif; ?>
   <div class="<?php if ($shaded){print 'shaded';} ?> <?php if ($subtitle) {print 'with_subtitle';}?> <?php if (isset($links_list)) {print 'with_lists';} ?>">
-    <?php if ($subtitle): ?><div class="subtitle"><?php print $subtitle; ?></div><?php endif; 
+    <?php if ($subtitle): ?>
+      <div class="subtitle"><?php print $subtitle; ?></div>
+   <?php endif; 
     if (!empty($links_list)): ?>
       <ul>
         <?php foreach($links_list as $link_from_list): ?><li><?php print $link_from_list; ?></li><?php endforeach; ?>
