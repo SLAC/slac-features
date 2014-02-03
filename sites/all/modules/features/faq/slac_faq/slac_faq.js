5bf1479c067bc5c8e4cccab51417f5492b1400de
Andrei Jechiu <andrei.jechiu@wearepropeople.com>
2014-02-03 12:13:25 +0200

    Task #10520: Use teasers for faqs.

--- sites/all/modules/features/faq/slac_faq/slac_faq.js
+++ sites/all/modules/features/faq/slac_faq/slac_faq.js
@@ -3,10 +3,10 @@
   "use strict";
   Drupal.behaviors.slac_faq = {};
   Drupal.behaviors.slac_faq.attach = function (context, settings) {
-    $('.view-id-faq .views-field-field-slac-faq-answer').hide().parent().addClass('inactive');
+    $('.view-id-faq .views-field-field-slac-faq-answer').hide().parent().parent().addClass('inactive');
     var $title = $('.view-id-faq .views-field-title');
     $title.click(function (event) {
-      $(this).parent().toggleClass('inactive');
+      $(this).parent().parent().toggleClass('inactive');
       $(this).parent().find('.views-field-field-slac-faq-answer').slideToggle("fast");
       event.stopPropagation();
     });
---
+++ sites/all/themes/slac/templates/node--slac_faq--teaser.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a node.
+ */
+?>
+
+<div class="faq contextual-links-region">
+  <div class="views-field views-field-title">
+    <span class="field-content"><?php print $title; ?></span>
+  </div>
+  <?php print render($title_suffix); ?>
+  <div class="views-field-field-slac-faq-answer">
+    <span class="field-content"><?php print $field_slac_faq_answer[0]['value']; ?></span>
+  </div>
+</div>
+
