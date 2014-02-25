1ffdd3cb888f08e87176310e57f4911811c579a7
Andrei Jechiu <andrei.jechiu@wearepropeople.com>
2014-02-25 17:31:12 +0200

    Make wiki node title as link for teasers.

--- sites/all/themes/slac/templates/node--slac_wiki_page.tpl.php
+++ sites/all/themes/slac/templates/node--slac_wiki_page.tpl.php
@@ -13,7 +13,7 @@
     <header>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
-        <h2 class='separator' <?php print $title_attributes; ?>><?php print $title; ?></h2>
+        <h2 class='separator' <?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
 
