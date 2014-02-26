84448b48284a70c174fbc1ed20efdfc3df36e9e3
Andrei Jechiu <andrei.jechiu@wearepropeople.com>
2014-02-26 12:18:11 +0200

    Task #11188: Add description for images in node body.

--- sites/all/modules/custom/pp_lightbox/js/pp_lightbox-wysiwyg-media.js
+++ sites/all/modules/custom/pp_lightbox/js/pp_lightbox-wysiwyg-media.js
@@ -5,6 +5,7 @@
     // variable there so we can set up default value of the checkbox in
     // pp_lightbox_form_media_format_form_alter() function.
     parameters.info.fields.pp_lightbox = $('#mediaStyleSelector').contents().find('input[name=pp_lightbox]').is(':checked');
+    parameters.info.fields.pp_description = $('#mediaStyleSelector').contents().find('input[name=pp_description]').is(':checked');
   });
 
 })(jQuery);
---
+++ sites/all/modules/custom/pp_lightbox/pp-image-style-description.tpl.php
@@ -0,0 +1,11 @@
+<?php
+?>
+
+<div class="pp-image-style-description">
+  <div class="pp-image-style-description-content">
+    <?php print $content; ?>
+  </div>
+  <div class="pp-image-style-description-description">
+    <?php print $description; ?>
+  </div>
+</div>
