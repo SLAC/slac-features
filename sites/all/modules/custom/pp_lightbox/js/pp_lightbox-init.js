8dd4e6c954db538c981f9ffc09fda00e0c128436
Eugen Taracila <taracilaeugen@gmail.com>
2014-02-28 14:58:43 +0200

    Bug #11188 : Ability to create page with no left navigation for all provisioned sites.

--- sites/all/modules/custom/pp_lightbox/js/pp_lightbox-init.js
+++ sites/all/modules/custom/pp_lightbox/js/pp_lightbox-init.js
@@ -3,7 +3,6 @@
     attach: function(context) {
       if($('.pp-image-style-description')[0]) {
         var imgWidth = $('.pp-image-style-description img').attr('width');
-        console.log(imgWidth);
         if(imgWidth > 0) {
           $('.pp-image-style-description').css('width', imgWidth)
         }
