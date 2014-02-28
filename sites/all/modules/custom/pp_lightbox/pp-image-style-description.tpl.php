f95ec8a8b1ac521458ce13cb2bf954631bf2717e
Andrei Jechiu <andrei.jechiu@wearepropeople.com>
2014-02-28 12:09:01 +0200

    Image position template fixes.

--- sites/all/modules/custom/pp_lightbox/pp-image-style-description.tpl.php
+++ sites/all/modules/custom/pp_lightbox/pp-image-style-description.tpl.php
@@ -1,11 +1,11 @@
 <?php
 ?>
 
-<div class="pp-image-style-description">
-  <div class="pp-image-style-description-content">
+<div class="pp-image-style-description pp-image-style-description-<?php print $position; ?>">
+  <div class="pp-image-style-description-content pp-image-style-description-content-<?php print $position; ?>">
     <?php print $content; ?>
   </div>
-  <div class="pp-image-style-description-description">
+  <div class="pp-image-style-description-description pp-image-style-description-description-<?php print $position; ?>">
     <?php print $description; ?>
   </div>
 </div>
