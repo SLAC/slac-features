3b1727d742596c136391a8e0bf00a307dfbfc0e8
Travis Dacolias <travis@wearepropeople.com>
2014-03-06 15:20:42 -0700

    Issue #11364: Rebuilt accordian functionality for new faq.

---
+++ sites/all/modules/features/faq/slac_faq/slac_faq.js
@@ -0,0 +1,14 @@
+// Javascript document
+(function ($, Drupal) {
+  "use strict";
+  Drupal.behaviors.slac_faq = {};
+  Drupal.behaviors.slac_faq.attach = function (context, settings) {
+    $('.view-id-slac_faq .views-field-field-slac-faq-answer').hide().parent().parent().addClass('inactive');
+    var $title = $('.view-id-slac_faq .views-field-title');
+    $title.click(function (event) {
+      $(this).parent().parent().toggleClass('inactive');
+      $(this).parent().find('.views-field-field-slac-faq-answer').slideToggle("fast");
+      event.stopPropagation();
+    });
+  };
+}(this.jQuery, this.Drupal));
