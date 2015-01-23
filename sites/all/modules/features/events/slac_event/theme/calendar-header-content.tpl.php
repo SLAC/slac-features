4d0b41791fb6d574c6e96ff07a3c8563c3e02fd6
Robyn Green <robyn.green@gmail.com>
2015-01-22 18:51:06 -0600

    Configured toggle for list/grid and date view

---
+++ sites/all/modules/features/events/slac_event/theme/calendar-header-content.tpl.php
@@ -0,0 +1,16 @@
+<div class="events-calendar-header">
+  <div class="events-calendar-header-pager">
+    <?php print $pager; ?>
+  </div>
+  <div class="events-calendar-header-date">
+    <?php print $date_display; ?>
+  </div>
+  <div class="events-calendar-header-date">
+    <div class="events-calendar-header-switch">
+      Switch to: <?php print $switch; ?>
+    </div>
+    <div class="events-calendar-header-displayview">
+      <?php print $display_modes; ?>
+    </div>
+  </div>
+</div>
