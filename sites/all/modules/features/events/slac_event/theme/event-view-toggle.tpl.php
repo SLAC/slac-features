bebb430f4d24d27765aad34d46872bb2baa77c1b
Robyn Green <robyn.green@gmail.com>
2015-02-13 14:42:41 -0600

    moved view toggle from view handler to cotent type plugin

---
+++ sites/all/modules/features/events/slac_event/theme/event-view-toggle.tpl.php
@@ -0,0 +1,10 @@
+<div class="events-calendar-toggle">
+  <div class="events-calendar-toggle-date-switch">
+    <div class="events-calendar-toggle-switch">
+      Switch to: <?php print $switch; ?>
+    </div>
+    <div class="events-calendar-toggle-displayview">
+      <?php print $display_modes; ?>
+    </div>
+  </div>
+</div>
