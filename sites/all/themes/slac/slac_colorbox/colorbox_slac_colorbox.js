ac57c2d55bab532e7b9c23986cae091a5e8c0235
Will Hartmann <will.hartmann@vtpventures.com>
2015-06-29 08:47:25 -0700

    WSFO-25: Added JS to center cboxCurrent

--- sites/all/themes/slac/slac_colorbox/colorbox_slac_colorbox.js
+++ sites/all/themes/slac/slac_colorbox/colorbox_slac_colorbox.js
@@ -3,18 +3,14 @@
 Drupal.behaviors.initColorboxDefaultStyle = {
   attach: function (context, settings) {
     $(context).bind('cbox_complete', function () {
-      // Only run if there is a title.
-      if ($('#cboxTitle:empty', context).length == false) {
-        $('#cboxLoadedContent img', context).bind('mouseover', function () {
-          $('#cboxTitle', context).slideDown();
-        });
-        $('#cboxOverlay', context).bind('mouseover', function () {
-          $('#cboxTitle', context).slideUp();
-        });
-      }
-      else {
-        $('#cboxTitle', context).hide();
-      }
+      // Override the default setting to allow the close button to overflow
+      $('#colorbox', context).css('overflow', 'visible');
+
+      // Center the Image X of Y
+      var width = $('#cboxCurrent').width();
+      var parentWidth = $('#cboxCurrent').offsetParent().width();
+      var left = (parentWidth - width) / 2;
+      $('#cboxCurrent', context).css('left', left);
     });
   }
 };
