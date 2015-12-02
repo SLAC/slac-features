8f41333d7fdcfe116a18ffd2f098892370b74518
Vincent Flesouras <x.algorithm@gmail.com>
2015-12-02 23:57:50 +0000

    post update - pre patching

--- sites/all/modules/contrib/epsacrop/js/epsacrop.js
+++ sites/all/modules/contrib/epsacrop/js/epsacrop.js
@@ -1,50 +1,57 @@
-(function ($) {
+(function($) {
   Drupal.EPSACrop = {
     api: null,
     preset: null,
     delta: null,
     presets: {},
     init: false,
-    dialog: function (type_name, field_name, bundle, delta, img, trueSize) {
-      $('body').find('#EPSACropDialog').remove().end().append('<div title="'+ Drupal.t("Cropping Image") +'" id="EPSACropDialog"></div>');
+    dialog: function(type_name, field_name, bundle, delta, img, trueSize) {
+      $('body').find('#EPSACropDialog').remove().end().append('<div title="' + Drupal.t("Cropping Image") + '" id="EPSACropDialog"></div>');
 
       // Translatables buttons
       var buttons = {};
       var saveLabel = Drupal.t("Apply crop");
       var cancelLabel = Drupal.t("Cancel");
 
-      buttons[saveLabel] = function (){
-        $.post(Drupal.settings.basePath +'?q=crop/ajax/put/' + delta, {'coords': JSON.stringify(Drupal.EPSACrop.presets)});
+      // Support multilingual sites with path prefixes. It's either this or
+      // support for non-clean URL's, we can't have both.
+      var pathPrefix = '?q=';
+      if (Drupal.settings.pathPrefix !== undefined && Drupal.settings.pathPrefix != '') {
+        pathPrefix = Drupal.settings.pathPrefix;
+      }
+
+      buttons[saveLabel] = function() {
+        $.post(Drupal.settings.basePath + pathPrefix + 'crop/ajax/put/' + delta, {'coords': JSON.stringify(Drupal.EPSACrop.presets)});
         var field = field_name.replace(/_/g, '-');
         var welem = $('div[id*="' + field + '"]').eq(0);
-        if(welem.find('.warning').size() == 0) {
+        if (welem.find('.warning').size() == 0) {
           welem.prepend('<div class="tabledrag-changed-warning messages warning">' + Drupal.t("Changes made in image crops will not be saved until the form is submitted.") + '</div>');
         }
         $(this).dialog('close');
         $('#EPSACropDialog').remove();
       };
-      buttons[cancelLabel] = function () {
+      buttons[cancelLabel] = function() {
         $(this).dialog('close');
         $('#EPSACropDialog').remove();
       };
 
       $('#EPSACropDialog').dialog({
-          bgiframe: true,
-          height: 600,
-          width: 850,
-          modal: true,
-          draggable: false,
-          resizable: false,
-          overlay: {
-            backgroundColor: '#000',
-            opacity: 0.6
-          },
-          buttons: buttons,
-          close: function () {
-            $('#EPSACropDialog').remove();
-          }
-        }).load(Drupal.settings.basePath +'?q=crop/dialog/' + type_name + '/' + field_name +'/' + bundle +'/'+ delta, function (){
-        try{
+        bgiframe: true,
+        height: 600,
+        width: 850,
+        modal: true,
+        draggable: false,
+        resizable: false,
+        overlay: {
+          backgroundColor: '#000',
+          opacity: 0.6
+        },
+        buttons: buttons,
+        close: function() {
+          $('#EPSACropDialog').remove();
+        }
+      }).load(Drupal.settings.basePath + pathPrefix + 'crop/dialog/' + type_name + '/' + field_name + '/' + bundle + '/' + delta, function() {
+        try {
           var item = $('.epsacrop-presets-menu a[class=selected]');
           var preset = item.attr('id');
           var coords = item.attr('rel').split('x');
@@ -63,20 +70,21 @@
 
           presets = Drupal.EPSACrop.presets || {};
           if (Drupal.EPSACrop.init === false && $('#epsacrop-coords-data').val().length > 0) {
-            presets = JSON.parse( $('#epsacrop-coords-data').val()) || {};
+            presets = JSON.parse($('#epsacrop-coords-data').val()) || {};
             Drupal.EPSACrop.init = true;
           }
 
-          if ((typeof presets[delta] == 'object') && (typeof presets[delta][preset] == 'object') ) {
+          if ((typeof presets[delta] == 'object') && (typeof presets[delta][preset] == 'object')) {
             var c = presets[delta][preset];
           }
 
           // Change the ratio into numeric
-          if(aspectRatio.length > 0) {
-            if(aspectRatio.split('/').length == 0) {
+          if (aspectRatio.length > 0) {
+            if (aspectRatio.split('/').length == 0) {
               ratios = aspectRatio.split('/');
               aspectRatio = parseInt(ratios[0]) / parseInt(ratios[1]);
-            }else{
+            }
+            else {
               aspectRatio = parseFloat(aspectRatio);
             }
           }
@@ -88,7 +96,7 @@
           target.load(function() {
             targetWait.hide();
             Drupal.EPSACrop.api = $.Jcrop('#' + target.attr('id'), {
-              aspectRatio: (aspectRatio.length > 0) ? aspectRatio : (w/h),
+              aspectRatio: (aspectRatio.length > 0) ? aspectRatio : (w / h),
               trueSize: trueSize,
               onSelect: Drupal.EPSACrop.update,
               bgColor: bgcolor,
@@ -98,15 +106,22 @@
             // animateTo, to avoid one bug from Jcrop I guess,
             // He doesn't calculate the scale with setSelect at the begining, so
             // I add animateTo after initate the API.
-            Drupal.EPSACrop.api.animateTo(((typeof c == 'object') ? [c.x, c.y, c.x2, c.y2] : [0, 0, w, h]));
+            Drupal.EPSACrop.api.animateTo(((typeof c == 'object') ? [c.x, c.y, c.x2, c.y2] : [0, 0, w, h]), function() {
+              if (typeof presets[delta] == 'undefined') {
+                presets[delta] = {};
+              }
+              if (typeof presets[delta][preset] == 'undefined') {
+                presets[delta][preset] = Drupal.EPSACrop.api.tellSelect();
+              }
+            });
           });
           Drupal.EPSACrop.presets = presets || {};
-        }catch(err) {
+        } catch (err) {
           alert(Drupal.t("Error on load : @error", {'@error': err.message}));
         }
       }); // end load
     }, // end dialog
-    crop: function ( preset ) {
+    crop: function(preset) {
       $('.epsacrop-presets-menu a').removeClass('selected');
       $('.epsacrop-presets-menu a#' + preset).addClass('selected');
 
@@ -122,35 +137,44 @@
       var h = parseInt(coords[1]);
 
       // Change the ratio in numeric
-      if(aspectRatio.length > 0) {
-        if(aspectRatio.split('/').length == 0) {
+      if (aspectRatio.length > 0) {
+        if (aspectRatio.split('/').length == 0) {
           ratios = aspectRatio.split('/');
           aspectRatio = parseInt(ratios[0]) / parseInt(ratios[1]);
-        }else{
+        }
+        else {
           aspectRatio = parseFloat(aspectRatio);
         }
       }
 
       Drupal.EPSACrop.preset = preset;
 
-      if(typeof presets[delta] == 'object' && typeof presets[delta][preset] == 'object' ) {
+      if (typeof presets[delta] == 'object' && typeof presets[delta][preset] == 'object') {
         var c = presets[delta][preset];
         Drupal.EPSACrop.api.animateTo([c.x, c.y, c.x2, c.y2]);
-      }else{
-        Drupal.EPSACrop.api.animateTo([0, 0, w, h]);
+      }
+      else {
+        Drupal.EPSACrop.api.animateTo([0, 0, w, h], function() {
+          if (typeof presets[delta] == 'undefined') {
+            presets[delta] = {};
+          }
+          if (typeof presets[delta][preset] == 'undefined') {
+            presets[delta][preset] = Drupal.EPSACrop.api.tellSelect();
+          }
+        });
       }
       Drupal.EPSACrop.api.setOptions({
-        aspectRatio: (aspectRatio.length > 0) ? aspectRatio : (w/h),
+        aspectRatio: (aspectRatio.length > 0) ? aspectRatio : (w / h),
         bgColor: bgcolor,
         bgOpacity: bgopacity
       });
     },
-    update: function ( c ) {
+    update: function(c) {
       var preset = Drupal.EPSACrop.preset;
       var delta = Drupal.EPSACrop.delta;
       var presets = Drupal.EPSACrop.presets || {};
 
-      if(typeof presets[delta] != 'object') {
+      if (typeof presets[delta] != 'object') {
         presets[delta] = {};
       }
 
